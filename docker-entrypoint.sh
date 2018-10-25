67bfe2d0369bdd4180a27116320f4a2463518f21
Chris Fordham <cfordham@scentregroup.com>
2018-10-25 17:03:02 +1100

    Forward proxy support in entrypoint.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,8 @@ if [ ! -z "$AUR_PACKAGES" ]; then
   aurman -S --noedit --noconfirm --pgp_fetch $AUR_PACKAGES
 fi
 
+# set proxy to system if needed
+[ ! -z "$http_proxy" ] && echo "http_proxy=$http_proxy" >> /etc/environment
+[ ! -z "$https_proxy" ] && echo "https_proxy=$https_proxy" >> /etc/environment
+
 echo "> $@" && exec "$@"
